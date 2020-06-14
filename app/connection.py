cd8a2c698803f1d975d0b621721b4cdecb2c2f37
ullyzian <aviev2000@icloud.com>
2020-06-14 16:26:55 +0200

    Heroku database added

--- app/connection.py
+++ app/connection.py
@@ -1,10 +1,14 @@
 import psycopg2
 
 
+# conn = psycopg2.connect(user="viktor", password="password",
+#                         host="127.0.0.1", port="5432", database="chat_app")
+
+
 def create_connection():
     try:
-        conn = psycopg2.connect(user="viktor", password="password",
-                                host="127.0.0.1", port="5432", database="chat_app")
+        conn = psycopg2.connect(user="tulqubamntpngi", password="c9b2a29b19ee3793f4d96226f0771cf4f239414ede9bb0889dfa39b988c54350",
+                                host="ec2-54-247-118-139.eu-west-1.compute.amazonaws.com", port="5432", database="d7tuo8j8i2h2kd")
         cursor = conn.cursor()
         return cursor, conn
     except (Exception, psycopg2.Error) as error:
